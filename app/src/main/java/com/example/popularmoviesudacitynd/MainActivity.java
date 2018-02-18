c04fe21851c38d450916c5ce59f6a7c8761674ba
Piotr Prus <prus.piotr@gmail.com>
2018-02-18 22:56:33 +0100

    prepare MainActivity base on mvp

--- app/src/main/java/com/example/popularmoviesudacitynd/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesudacitynd/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.popularmoviesudacitynd;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
-public class MainActivity extends AppCompatActivity {
+import com.hannesdorfmann.mosby.mvp.MvpPresenter;
+
+public class MainActivity extends BaseMvpActivity {
+
+    @Override
+    public String getToolbarTitle() {
+        //TODO: Set toolbar title
+        return "test test";
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @NonNull
+    @Override
+    public MvpPresenter createPresenter() {
+        //TODO Make presenter
+        return null;
     }
 }
